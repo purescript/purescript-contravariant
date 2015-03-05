739081ae579f653a7039d2a866edc62c7e087f6a
Gary Burgess <gary.burgess@gmail.com>
2015-03-05 14:18:08 +0000

    Fix bower.json
    Add flipped contramap
    
    Update grunt plugin
    
    Add Comparison, Equivalence, Predicate
    
    Relax Ord constraint to Eq
    
    Move under Data.Functor
    
    Better contramap implementations
    
    Add Compose and friends
    
    Update docs
    
    Update docs
    
    Remove compose

---
+++ src/Data/Functor/Contravariant.purs
@@ -0,0 +1,17 @@
+module Data.Functor.Contravariant where
+
+-- | A `Contravariant` functor can be seen as a way of changing the input type
+-- | of a consumer of input, in contrast to the standard covariant `Functor`
+-- | that can be seen as a way of changing the output type of a producer of
+-- | output.
+-- |
+-- | `Contravariant` instances should satisfy the following laws:
+-- |
+-- | - Identity `(>$<) id = id`
+-- | - Composition `(f >$<) <<< (g >$<) = (>$<) (g <<< f)`
+class Contravariant f where
+  (>$<) :: forall a b. (b -> a) -> f a -> f b
+
+-- | `(>#<)` is `(>$<)` with its arguments reversed.
+(>#<) :: forall a b f. (Contravariant f) => f a -> (b -> a) -> f b
+(>#<) x f = f >$< x
